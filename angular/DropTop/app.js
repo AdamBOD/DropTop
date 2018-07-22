96d58f639c740541d4e267be8819f6e90d722485
AdamBOD <adambarryodonovan@gmail.com>
2018-07-22 16:52:40 +0100

    Updated API

--- angular/DropTop/app.js
+++ angular/DropTop/app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 
 var apiRouter = require('./routes/userData');
-var loginRouter = require('./routes/login');
+var loginRouter = require('./routes/loginData');
 var mongoose = require('mongoose');
 
 var app = express();
@@ -15,11 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist/DropTop')));
 app.use('/', express.static(path.join(__dirname, 'dist/DropTop')));
-app.use('/login', loginRouter);
-app.use('/api', apiRouter);
+// app.use('/login', loginRouter);
+// app.use('/api', apiRouter);
 
 mongoose.connect('mongodb://localhost/DropTop', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection successful'))
+  .then((data) =>  {
+    console.log('connection successful')
+  })
   .catch((err) => console.error(err));
 
 // catch 404 and forward to error handler
@@ -36,6 +38,30 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.send(err.status);
+});
+
+app.get ('api/userData', (req, res) => {
+
+});
+
+app.post ('api/userData', (req, res) => {
+
+});
+
+app.put ('api/userData', (req, res) => {
+
+});
+
+app.get ('api/login', (req, res) => {
+  res.send ("Login Portal");
+});
+
+app.post ('api/login', (req, res) => {
+  res.send ("Login");
+});
+
+app.put ('api/login', (req, res) => {
+  res.send ("Register");
 });
 
 app.get('*', (req, res) => {
--- angular/DropTop/models/loginData.js
+++ angular/DropTop/models/loginData.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var loginDetailsData = new mongoose.Schema ({
     username: String,
-    password: String
+    password: String,
+    currentSessions: []
 });
 
-module.exports = mongoose.model('loginDetailsData', loginDetailsData);
+module.exports = mongoose.model('loginData', loginDetailsData);
